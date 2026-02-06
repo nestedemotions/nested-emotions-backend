bfb06d773861e9442699f8736b14f200b5aaaad4
nestedemotions <nestedemotions@gmail.com>
2026-02-06 17:49:24 +0530

    fix-db-connection

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/health", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
